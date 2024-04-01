0551bfbb704896abcc520c9ee4b729b0e85fb52a
Nicole Souza <nicoledesouza062@gmail.com>
2024-04-01 16:58:08 -0300

    Sexto Commit

--- js/script.js
+++ js/script.js
@@ -1,97 +1,175 @@
-//PRINCIPAIS OBJETOS//
+//Principais objetos//
 let addNote = document.querySelector('#add-note');//Botão de para adicionar nota
 let btnCloseModal =  document.querySelector('#btn-close-modal'); //fechar janela modal com os detalhes da nota.
 let modal = document.querySelector('#modal'); //Modal para edição das notas
 let modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
 let notes = document.querySelector('#notes');//Lista divs com dados das notas
 let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
-//let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
-
-//EVENTOS//
+let btnCloseNote = document.querySelector("#close-modal-view");//icone para fechar modal de edição de nota.
+//Eventos//
 
 //Adicionar nova anotação//
-addNote.addEventListener('click', (evt)=>{
+addNote.addEventListener('click',
+(evt)=>{
   evt.preventDefault();
-  modal.style.display='block';
+  modal.style.display="block";
   addNote.style.display = "none";
   notes.style.display = "none";
 });
 
-//Fechar//
-btnCloseModal.addEventListener("click", (evt) => {
+//Botão fechar//
+btnCloseModal.addEventListener("click",(evt) =>{
   evt.preventDefault();
-  modal.style.display = "none";
+  modal.style.display="none";
   addNote.style.display = "block";
   notes.style.display = "flex";
-});
+})
 
-//Salvar anotações//
-btnSaveNote.addEventListener("click", (evt) => {
+//Salvar anotação//
+btnSaveNote.addEventListener("click", (evt) =>{
   evt.preventDefault();
-  data = {
-  id: document.querySelector("#input-id").value, 
-  title: document.querySelector("#input-title").value, 
-  content: document.querySelector("#input-content").value
-}
-saveNote(data);
+  data = { 
+    id: document.querySelector("#input-id").value,
+    title: document.querySelector("#input-title").value,
+    content: document.querySelector("#input-content").value
+  }
+  saveNote(data);
+
 });
 
-//FUNÇÕES//
+btnCloseNote.addEventListener("click", (evt) =>{
+  evt.preventDefault();
+  modal.style.display="none";
+  addNote.style.display = "block";
+  notes.style.display = "flex";
+  modalView.style.display = 'none';
+})
+
+//Funções//
 const saveNote = (note) => {
-let notes = loadNotes();
-  if(note.id.trim().length < 1) {
-    note.id = new Date().getTime();
-  }
-  else{
-    //?
-  }
-
+  let notes = loadNotes();
   note.lastTime = new Date().getTime();
-  console.log(note);
-  notes.push(note);
-  notes = JSON.stringify(notes);
-  localStorage.setItem('notes', notes);
-};
+    if(note.id.trim().length < 1) {
+      note.id = new Date().getTime();
+      notes.push(note);
+      document.querySelector('#input-id').value = note.id;
+    }
+    else{
+      notes.forEach((item, i) =>{
+      if(item.id == note.id){
+        notes[i] = note;
+      }
+    });
+    }
+  
+    console.log(note);
+   
+    notes = JSON.stringify(notes);
+    localStorage.setItem('notes', notes);
 
-const loadNotes = () => {
-  let notes = localStorage.getItem('notes');
-  if(!notes) {
-    notes = [];
+    listNotes();
+  };
+  
+  
+  const loadNotes = () => {
+    let notes = localStorage.getItem('notes');
+    if(!notes) {
+      notes = [];
+    }
+    else{
+      notes = JSON.parse(notes);
+    }
+    return notes;
   }
-  else{
-    notes = JSON.parse(notes);
+  
+  
+  const listNotes = ( ) => {
+    notes.innerHTML = '';
+      let listNotes = loadNotes();
+      listNotes.forEach((note) => {
+          let divCard = document.createElement('div');
+          divCard.className = 'card';
+          divCard.style.width = '25rem';
+          let divCardBody = document.createElement('div');
+          divCardBody.className = 'card-body';
+          divCard.appendChild(divCardBody);
+          let h5 = document.createElement('h5');
+          h5.innerText = note.title;
+          divCardBody.appendChild(h5);
+  
+  
+          notes.appendChild(divCard);
+  
+  
+          let p = document.createElement('p');
+          p.innerText = note.content;
+          divCardBody.appendChild(p);
+  
+  
+          let plastTime = document.createElement('p')
+          plastTime.innerText = "Atualizado em: "+dateFormat(note.lastTime);
+          divCardBody.appendChild(plastTime);
+  
+  
+          notes.appendChild(divCard);
+
+          divCard.addEventListener('click', (evt) => {
+            showNote(note);
+
+          
+
+
+          });
+      });
+    }
+     
+  
+    const showNote = (note) =>{
+      document.querySelector('#controls-note').innerHTML = '';
+      notes.style.display = 'none';
+      addNote.style.display = 'none';
+      modalView.style.display = 'block';
+      document.querySelector('#title-note').innerHTML = "<h1>"+note.title+"</h1>";
+      document.querySelector('#content-note').innerHTML = `<p>${note.content}</p> <p>Última Alteração : ${dateFormat(note.lastTime)}</p>`
+      let divEdit = document.createElement("div");
+      let iEdit = document.createElement("i");
+      iEdit.className = 'bi bi-pen';
+      divEdit.appendChild(iEdit);
+      document.querySelector("#controls-note").appendChild(divEdit);
+  
+  
+      divEdit.addEventListener("click", (evt)=> {
+        evt.preventDefault();
+        document.querySelector("#input-id").value = note.id;
+        document.querySelector("#input-title").value = note.title;
+        document.querySelector("#input-content").value = note.content;
+        modal.style.display="block";
+        addNote.style.display = "none";
+        notes.style.display = "none";
+        modalView.style.display = 'none';
+      })
+  
+  
+      let divExc = document.createElement("div");
+      let iExc = document.createElement("i");
+      iExc.className = 'bi bi-trash3';
+      divExc.appendChild(iExc);
+      document.querySelector("#controls-note").appendChild(divExc);
+  
+  
+      divExc.addEventListener("click", (evt) =>{
+        evt.preventDefault();
+        modal.style.display="none";
+        addNote.style.display = "block";
+        notes.style.display = "none";
+        modalView.style.display = 'none';
+      })
+    }
+
+  const dateFormat = (timestamp) =>{
+  let date = new Date(timestamp); //converte para data
+  date = date.toLocaleDateString("pt-BR");
+  return date;
   }
-  return notes;
-}
 
-const listNotes = ( ) => {
-  let listNotes = loadNotes();
-  listNotes.forEach((note) => {
-    let divCard = document.createElement('div');
-    divCard.className = 'card';
-    divCard.style.width = '25rem';
-    let divCardBody = document.createElement('div');
-    divCardBody.className = 'card-body';
-    divCard.appendChild(divCardBody);
-    let h5 = document.createElement('h5');
-    h5.innerText = note.title;
-    divCardBody.appendChild(h5);
-    let time = new Date(note.lastTime); //converter para data
-    time=time.toLocaleDateString("pt-BR");
-    console.log(time)
-
-    notes.appendChild(divCard);
-
-    let p = document.createElement('p');
-    p.innerText = note.content;
-    divCardBody.appendChild(p);
-
-    let plastTime = document.createElement('p')
-    plastTime.innerText = "Aualizado em: "+time;
-    divCardBody.appendChild(plastTime);
-
-    notes.appendChild(divCard);
-  });
-}
-
-listNotes();
+  listNotes();
